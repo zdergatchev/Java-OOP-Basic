b72b26e0a8d5079be38004c25f1e267c6b04fc2d
limerixtt123 <limerix@mail.bg>
2017-03-06 13:51:15 +0200

    Encapsulation Lab

---
+++ 03EncapsulationLab/SortByNameAndAge/Main.java
@@ -0,0 +1,36 @@
+package SortByNameAndAge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+
+        List<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+            persons.add(new Person(input[0], input[1], Integer.parseInt(input[2])));
+        }
+
+        Collections.sort(persons, (firstPerson, secondPerson) -> {
+            int sComp = firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
+
+            if (sComp != 0) {
+                return sComp;
+            } else {
+                return firstPerson.getAge().compareTo(secondPerson.getAge());
+            }
+        });
+
+        for (Person person : persons) {
+            System.out.println(person.toString());
+        }
+    }
+}
