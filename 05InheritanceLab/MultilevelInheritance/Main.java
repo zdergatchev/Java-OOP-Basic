5728db1169639f2b7e7cfe7b6f1c90d916039b58
limerixtt123 <limerix@mail.bg>
2017-03-06 14:25:56 +0200

    Inheritance Lab

---
+++ 05InheritanceLab/MultilevelInheritance/Main.java
@@ -0,0 +1,10 @@
+package MultilevelInheritance;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Puppy puppy = new Puppy();
+        puppy.eat();
+        puppy.bark();
+        puppy.weep();
+    }
+}
