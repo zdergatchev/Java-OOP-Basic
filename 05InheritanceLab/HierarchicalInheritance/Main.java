6d39076d5f13d4ccb98dda2e575ad2cd088d1762
limerixtt123 <limerix@mail.bg>
2017-03-06 14:25:00 +0200

    Inheritance Lab

---
+++ 05InheritanceLab/HierarchicalInheritance/Main.java
@@ -0,0 +1,13 @@
+package HierarchicalInheritance;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+    }
+}
