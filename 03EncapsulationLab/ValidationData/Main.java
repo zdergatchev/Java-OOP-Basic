422d40668928dd4a7063723c113b22eaba39709d
limerixtt123 <limerix@mail.bg>
2017-03-06 13:54:02 +0200

    Encapsulation Lab

---
+++ 03EncapsulationLab/ValidationData/Main.java
@@ -0,0 +1,37 @@
+package ValidationData;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+
+        List<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+            try{
+                Person person = new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3]));
+                persons.add(person);
+            } catch (IllegalArgumentException ex){
+                System.out.println(ex.getMessage());
+            }
+
+        }
+
+//        if(!persons.isEmpty() || persons.size() == n){
+//            System.out.println("--------------------");
+//        }
+
+        int bonus = Integer.parseInt(reader.readLine());
+        for (Person person : persons) {
+            person.increaseSalary((double)bonus);
+            System.out.println(person.toString());
+        }
+    }
+}
