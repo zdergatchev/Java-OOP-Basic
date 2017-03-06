84774ecc864f288ce244fe53d70fe7f031eba629
limerixtt123 <limerix@mail.bg>
2017-03-06 14:27:38 +0200

    Inheritance Lab

---
+++ 05InheritanceLab/SingleInheritance/Main.java
@@ -0,0 +1,11 @@
+package SingleInheritance;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+       // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
