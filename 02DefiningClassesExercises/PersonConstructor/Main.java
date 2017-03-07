51be2cac4f30f281d799365f70c1afa85dd079be
limerixtt123 <limerix@mail.bg>
2017-03-07 10:20:38 +0200

    Defining Classes Exercises

---
+++ 02DefiningClassesExercises/PersonConstructor/Main.java
@@ -0,0 +1,31 @@
+package PersonConstructor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, IOException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(isr);
+
+        Class personClass = Person.class;
+        Constructor emptyCtor = personClass.getDeclaredConstructor();
+        Constructor ageCtor = personClass.getDeclaredConstructor(int.class);
+        Constructor nameAgeCtor = personClass.getDeclaredConstructor(String.class, int.class);
+        String name = reader.readLine();
+
+        int age = Integer.parseInt(reader.readLine());
+
+        Person basePerson = (Person) emptyCtor.newInstance();
+        Person personWithAge = (Person) ageCtor.newInstance(age);
+        Person personFull = (Person) nameAgeCtor.newInstance(name, age);
+
+        System.out.printf("%s %s%n", basePerson.getName(), basePerson.getAge());
+        System.out.printf("%s %s%n", personWithAge.getName(), personWithAge.getAge());
+        System.out.printf("%s %s%n", personFull.getName(), personFull.getAge());
+    }
+}
