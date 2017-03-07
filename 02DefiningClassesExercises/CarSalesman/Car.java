3be76492124a4319839c2c934f1a521b2de9a111
limerixtt123 <limerix@mail.bg>
2017-03-07 10:11:10 +0200

    Defining Classes Exercises

---
+++ 02DefiningClassesExercises/CarSalesman/Car.java
@@ -0,0 +1,26 @@
+package CarSalesman;
+
+public class Car {
+    private String model;
+    private Engine engine;
+    private String weight = "n/a";
+    private String color = "n/a";
+
+    public Car(String model, Engine engine) {
+        this.model = model;
+        this.engine = engine;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s:%n%s%n  Weight: %s%n  Color: %s", this.model, this.engine, this.weight, this.color);
+    }
+}
