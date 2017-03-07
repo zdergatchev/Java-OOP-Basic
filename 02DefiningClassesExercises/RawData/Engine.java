c7c5f1b6b312cece7abd06f1847f4c2533a14e30
limerixtt123 <limerix@mail.bg>
2017-03-07 10:23:39 +0200

    Defining Classes Exercises

---
+++ 02DefiningClassesExercises/RawData/Engine.java
@@ -0,0 +1,19 @@
+package RawData;
+
+public class Engine {
+    private int speed;
+    private int power;
+
+    public Engine(int speed, int power) {
+        this.speed = speed;
+        this.power = power;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getPower() {
+        return power;
+    }
+}
