aa98e3c897856d2f84d6cdb237bbd2372f58ba67
limerixtt123 <limerix@mail.bg>
2017-03-07 10:20:10 +0200

    Defining Classes Exercises

---
+++ 02DefiningClassesExercises/Person/Person.java
@@ -0,0 +1,24 @@
+
+
+
+package Person;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+
+    public Person() {
+        this.name = name;
+    }
+
+    public Person(int age) {
+        this("No name", age);
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
