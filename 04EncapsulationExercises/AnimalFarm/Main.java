243147fbb4f89144589c936d6f464b4b3c02a43f
limerixtt123 <limerix@mail.bg>
2017-03-08 09:42:14 +0200

    Encapsulation Exercises

---
+++ 04EncapsulationExercises/AnimalFarm/Main.java
@@ -0,0 +1,21 @@
+package AnimalFarm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        int age = Integer.parseInt(reader.readLine());
+
+        try {
+            Chicken chicken = new Chicken(name, age);
+            System.out.println(chicken);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
