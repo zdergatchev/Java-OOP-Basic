65e39fee286d07a710c9a0c3e6a91b2f578258ab
limerixtt123 <limerix@mail.bg>
2017-03-09 10:35:10 +0200

    Inheritance Exercises

---
+++ 06InheritanceExercises/CompanyHierarchy/Department.java
@@ -0,0 +1,24 @@
+package CompanyHierarchy;
+
+public class Department {
+    private String name;
+
+    public Department(String name) {
+        this.setName(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().length() < 3 || (!name.equals("Production") && !name.equals("Accounting") &&
+                !name.equals("Sales") && !name.equals("Marketing"))) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.name = name;
+    }
+}
---
+++ 06InheritanceExercises/CompanyHierarchy/Main.java
@@ -0,0 +1,10 @@
+package CompanyHierarchy;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] inputTokens = scanner.nextLine().split("[\\s]+");
+    }
+}
