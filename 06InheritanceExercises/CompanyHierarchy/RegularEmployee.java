12307b88bf4d08d8a835bcc3c4e3b925217aa440
limerixtt123 <limerix@mail.bg>
2017-03-09 10:43:11 +0200

    Inheritance Exercises

--- 06InheritanceExercises/CompanyHierarchy/RegularEmployee.java
+++
@@ -1,7 +0,0 @@
-package CompanyHierarchy;
-
-public class RegularEmployee extends Employee {
-    public RegularEmployee(Integer id, String firstName, String lastName, Double salary, Department department) {
-        super(id, firstName, lastName, salary, department);
-    }
-}
