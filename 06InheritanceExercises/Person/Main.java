e73575cf62f00d0444ca4ca3c5220aa67387033b
limerixtt123 <limerix@mail.bg>
2017-03-09 10:40:29 +0200

    Inheritance Exercises

---
+++ 06InheritanceExercises/Person/Main.java
@@ -0,0 +1,20 @@
+package Person;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        Integer age = Integer.valueOf(reader.readLine());
+        try{
+            Child child = new Child(name, age);
+            System.out.println(child.toString());
+            String personClassName = Person.class.getSimpleName();
+            String childClassName = Child.class.getSimpleName();
+        } catch (IllegalArgumentException error){
+            System.out.println(error.getMessage());
+        }
+    }
+}
