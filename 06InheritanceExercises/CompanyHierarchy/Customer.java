b403764cf0788bee1a1407a6284547147e729d15
limerixtt123 <limerix@mail.bg>
2017-03-09 10:34:35 +0200

    Inheritance Exercises

---
+++ 06InheritanceExercises/CompanyHierarchy/Customer.java
@@ -0,0 +1,24 @@
+package CompanyHierarchy;
+
+public class Customer extends Person  {
+    private Double netPurchaseAmount;
+
+    public Customer(Integer id, String firstName, String lastName) {
+        super(id, firstName, lastName);
+        this.setNetPurchaseAmount(0D);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "netPurchaseAmount=" + netPurchaseAmount +
+                '}';
+    }
+
+    private void setNetPurchaseAmount(Double amount) {
+        if (amount == null || amount <= 0) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.netPurchaseAmount = amount;
+    }
+}
