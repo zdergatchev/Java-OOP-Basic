3a81c37d7ae609c5d4ec204ad458a8e8d4b54832
limerixtt123 <limerix@mail.bg>
2017-03-11 14:58:33 +0200

    BashSoft

--- BashSoft/BashSoft/src/StaticData/SessionData.java
+++
@@ -1,9 +0,0 @@
-package StaticData;
-
-import java.util.HashSet;
-
-public class SessionData {
-    public static String currentPath = System.getProperty("user.dir");
-    public static HashSet<Thread> threadPool = new HashSet<>();
-}
-
